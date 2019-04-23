0f6315fd4e5957c4990ef178d715b607b502a4a1
Shayan Sarnevesht <42shayan42@gmail.com>
2019-04-22 18:45:10 -0700

    Add files

---
+++ app/src/main/java/com/example/firebasetesting/Model.java
@@ -0,0 +1,35 @@
+package com.example.firebasetesting;
+
+public class Model {
+
+    String title, image, description;
+
+    //constructor
+    public Model(){}
+
+    //getter and setters press Alt+Insert
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
